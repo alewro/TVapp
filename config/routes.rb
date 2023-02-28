3b2bc6624684485633700529b20b56cf864d014a
Aleksandra <aleksandra@wroblewscy.eu>
2023-02-28 18:46:57 +0100

    Changing for date set

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   resources :stories
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root "pages#index"
   get 'manage', to: 'stories#manage'
-  post 'date', to: 'pages#date'
+  post '/', to: 'pages#date'
   post 'stories/:id', to: 'pages#counting'
- # get 'stories/:id/counting', to: 'pages#counting'
 end
