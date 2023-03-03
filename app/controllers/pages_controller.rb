166143f55e26c60fcd1c6cb7ae6cea5fa00dec45
Aleksandra <aleksandra@wroblewscy.eu>
2023-03-03 09:36:55 +0100

    Fix problem with authenticate token

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,4 @@
 class PagesController < ApplicationController
- skip_before_action :verify_authenticity_token, only: [ :counting ]
 
   def index
   end
@@ -7,8 +6,9 @@ class PagesController < ApplicationController
   def counting
     @story = Story.find(params[:id])
     @coupon = Coupon.last.quantity
-    @coupon = @coupon - @story.weight
+    @coupon -= @story.weight
     Coupon.last.update(quantity: @coupon)
     redirect_to stories_path
   end
+
 end
